91efe10aefe2eaee5c60e408dd060e1567c5221c
Alex da Costa <alex_investor@yahoo.com>
2017-05-11 18:36:39 -0400

    Images/animation/formatting

--- service/django/interface/static/apps/welcome/js/home.js
+++ service/django/interface/static/apps/welcome/js/home.js
@@ -17,7 +17,7 @@ new ScrollMagic.Scene({
  offset: 350 
 })
 .setTween("#lead_image_fade", 1.0, {opacity:0, ease: Power1.easeOut})
-.addIndicators({name:'lead_image_fade',colorStart:'yellow',colorTrigger:'green' })
+// .addIndicators({name:'lead_image_fade',colorStart:'yellow',colorTrigger:'green' })
 .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -26,9 +26,8 @@ new ScrollMagic.Scene({
  offset: 350 
 })
 .setTween("#lead_image_fade", 1.0, {opacity:0, ease: Power1.easeOut})
-.addIndicators({name:'lead_image_fade',colorStart:'yellow',colorTrigger:'green' })
+// .addIndicators({name:'lead_image_fade',colorStart:'yellow',colorTrigger:'green' })
 .addTo(controller);
-
 
 new ScrollMagic.Scene({
  triggerElement: '#intro_image',
@@ -36,7 +35,7 @@ new ScrollMagic.Scene({
  offset: 600 
 })
 .setTween("#intro_image", 1.0, {opacity: 0})
-.addIndicators({name:'intro_image',colorStart:'yellow',colorTrigger:'green' })
+// .addIndicators({name:'intro_image',colorStart:'yellow',colorTrigger:'green' })
 .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -44,13 +43,13 @@ new ScrollMagic.Scene({
  offset: 50 
 })
 .setTween("#intro_text", 1.0, {opacity:0, ease: Power0.easeNone})
-.addIndicators({name:'intro_text',colorStart:'blue',colorTrigger:'orange' })
+// .addIndicators({name:'intro_text',colorStart:'blue',colorTrigger:'orange' })
 .addTo(controller);
 
 new ScrollMagic.Scene({
  triggerElement: '.welcome_header_intro',
  duration: 400,
- offset: -150 
+ offset: -250 
 })
 .setTween(".welcome_header_intro", .5, {opacity:1, ease: Power2.easeOut})
 .addIndicators({name:'welcome_header_intro opacity',colorStart:'blue',colorTrigger:'orange' })
@@ -58,19 +57,56 @@ new ScrollMagic.Scene({
 
 new ScrollMagic.Scene({
  triggerElement: '.welcome_header_name',
- duration: 400,
- offset: -100 
+ duration: "50%",
+ offset: -150 
 })
 .setTween(".welcome_header_name", .5, {opacity:1, ease: Power2.easeOut})
-.addIndicators({name:'welcome_header_name opacity',colorStart:'blue',colorTrigger:'orange' })
+.addIndicators({name:'welcome_header_name opacity',colorStart:'orange',colorTrigger:'red' })
 .addTo(controller);
 
 var headerTween = TweenMax.staggerFromTo('.welcome_header_slogan', 1, { y: 50, x: -50, opacity: 0 }, { y: 0, x: 0, opacity: 1 });
 var headerScene = new ScrollMagic.Scene({
   triggerElement: '.welcome_header_slogan',
   duration: 200,
-  offset: -50
+  offset: -150
 })
 .setTween(headerTween)
-.addIndicators({name:'welcome_header_slogan',colorStart:'yellow',colorTrigger:'red' })
+.addIndicators({name:'welcome_header_slogan',colorStart:'red',colorTrigger:'yellow' })
 .addTo(controller);
+
+new ScrollMagic.Scene({
+ triggerElement: '.welcome_header_intro',
+ duration: "50%",
+ offset: -100
+})
+.setTween("#welcome_header_image", 1.0, {opacity:1, ease: Power1.easeOut})
+.addIndicators({name:'welcome_header_image_fadein',colorStart:'yellow',colorTrigger:'green' })
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: '.welcome_header_slogan',
+  duration: "25%",
+  offset: -150
+})
+.setTween("#welcome_header_image_overlay", 1.0, {opacity:0, ease: Power1.easeOut})
+.addIndicators({name:'welcome_header_image_overlay_fadeout',colorStart:'green',colorTrigger:'purple' })
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: '.welcome_header_slogan',
+  duration: "75%",
+  offset: 300 
+})
+.setTween("#welcome_header_image_overlay", 1.0, {opacity:1, ease: Power1.easeOut})
+.addIndicators({name:'welcome_header_image_overlay_fadeout',colorStart:'green',colorTrigger:'purple' })
+.addTo(controller);
+
+new ScrollMagic.Scene({
+ triggerElement: '.welcome_header_slogan',
+ duration: "50%",
+ offset: 500 
+})
+.setTween("#welcome", .5, {opacity:0, ease: Power2.easeOut})
+.addIndicators({name:'welcome opacity',colorStart:'purple',colorTrigger:'blue' })
+.addTo(controller);
+
