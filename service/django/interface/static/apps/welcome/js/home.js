a64ac8138855451ff280cdbde43e0daf9ef7f4b9
Alex da Costa <alex_investor@yahoo.com>
2019-02-03 16:48:16 -0500

    Chaning project designation to "application"

--- service/django/interface/static/apps/welcome/js/home.js
+++ service/django/interface/static/apps/welcome/js/home.js
@@ -7,26 +7,26 @@ $(document).ready(function(){
 
     var blogOffset = $('#welcome_header_slogan').offset().top;
     var photographyOffset = $('#blog_summary').offset().top - breakOffset;
-    var projectsOffset = $('#photography_summary').offset().top - breakOffset;
-    var endOffset = $('#projects_summary').offset().top - breakOffset
+    var appsOffset = $('#photography_summary').offset().top - breakOffset;
+    var endOffset = $('#apps_summary').offset().top - breakOffset
     if (pxToTop < blogOffset){
         jQuery('body').removeClass('blog_background');
         jQuery('body').removeClass('photography_background');
-        jQuery('body').removeClass('projects_background');
+        jQuery('body').removeClass('apps_background');
     }else if (pxToTop >= blogOffset && pxToTop < photographyOffset){
         jQuery('body').addClass('blog_background');
         jQuery('body').removeClass('photography_background');
-        jQuery('body').removeClass('projects_background');
-    }else if (pxToTop >= photographyOffset && pxToTop < projectsOffset){
+        jQuery('body').removeClass('apps_background');
+    }else if (pxToTop >= photographyOffset && pxToTop < appsOffset){
         jQuery('body').addClass('photography_background');
         jQuery('body').removeClass('blog_background');
-        jQuery('body').removeClass('projects_background');
-    }else if (pxToTop >= projectsOffset && pxToTop < endOffset){
-        jQuery('body').addClass('projects_background');
+        jQuery('body').removeClass('apps_background');
+    }else if (pxToTop >= appsOffset && pxToTop < endOffset){
+        jQuery('body').addClass('apps_background');
         jQuery('body').removeClass('photography_background');
         jQuery('body').removeClass('blog_background');
     }else{
-        jQuery('body').removeClass('projects_background');
+        jQuery('body').removeClass('apps_background');
         jQuery('body').removeClass('photography_background');
         jQuery('body').removeClass('blog_background');
     }
@@ -164,19 +164,19 @@ new ScrollMagic.Scene({
 .addTo(controller);
 
 new ScrollMagic.Scene({
- triggerElement: '#projects',
+ triggerElement: '#apps',
  duration: "50%",
  offset: -100 
 })
-.setTween("#projects_overlay", .5, {opacity:0, ease: Power1.easeOut})
-//.addIndicators({name:'projects_overlay_fadeIn',colorStart:'yellow',colorTrigger:'green' })
+.setTween("#apps_overlay", .5, {opacity:0, ease: Power1.easeOut})
+//.addIndicators({name:'apps_overlay_fadeIn',colorStart:'yellow',colorTrigger:'green' })
 .addTo(controller);
 
 new ScrollMagic.Scene({
- triggerElement: '#projects_summary',
+ triggerElement: '#apps_summary',
  duration: "50%",
  offset: -100 
 })
-.setTween("#projects_overlay", .5, {opacity:1, ease: Power1.easeOut})
-//.addIndicators({name:'projects_overlay_fadeOut',colorStart:'green',colorTrigger:'red' })
+.setTween("#apps_overlay", .5, {opacity:1, ease: Power1.easeOut})
+//.addIndicators({name:'apps_overlay_fadeOut',colorStart:'green',colorTrigger:'red' })
 .addTo(controller);
