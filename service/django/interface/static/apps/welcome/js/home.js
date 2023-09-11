6552786b5c6cd06d6f57e295add1ee9820103c3b
Alexandre da Costa <alexandre.dias.dacosta@gmail.com>
2023-09-11 00:15:47 -0400

    Fixed error due to name change.

--- service/django/interface/static/apps/welcome/js/home.js
+++ service/django/interface/static/apps/welcome/js/home.js
@@ -10,28 +10,28 @@ $(document).ready(function(){
     var breakOffset = $(window).height() * .25;
 
     var blogOffset = $('#welcome_header_slogan').offset().top;
-    var photographyOffset = $('#blog_summary').offset().top - breakOffset;
-    var appsOffset = $('#photography_summary').offset().top - breakOffset;
+    var albumOffset = $('#blog_summary').offset().top - breakOffset;
+    var appsOffset = $('#album_summary').offset().top - breakOffset;
     var endOffset = $('#apps_summary').offset().top - breakOffset
     if (pxToTop < blogOffset){
         jQuery('body').removeClass('blog_background');
-        jQuery('body').removeClass('photography_background');
+        jQuery('body').removeClass('album_background');
         jQuery('body').removeClass('apps_background');
-    }else if (pxToTop >= blogOffset && pxToTop < photographyOffset){
+    }else if (pxToTop >= blogOffset && pxToTop < albumOffset){
         jQuery('body').addClass('blog_background');
-        jQuery('body').removeClass('photography_background');
+        jQuery('body').removeClass('album_background');
         jQuery('body').removeClass('apps_background');
-    }else if (pxToTop >= photographyOffset && pxToTop < appsOffset){
-        jQuery('body').addClass('photography_background');
+    }else if (pxToTop >= albumOffset && pxToTop < appsOffset){
+        jQuery('body').addClass('album_background');
         jQuery('body').removeClass('blog_background');
         jQuery('body').removeClass('apps_background');
     }else if (pxToTop >= appsOffset && pxToTop < endOffset){
         jQuery('body').addClass('apps_background');
-        jQuery('body').removeClass('photography_background');
+        jQuery('body').removeClass('album_background');
         jQuery('body').removeClass('blog_background');
     }else{
         jQuery('body').removeClass('apps_background');
-        jQuery('body').removeClass('photography_background');
+        jQuery('body').removeClass('album_background');
         jQuery('body').removeClass('blog_background');
     }
 });
@@ -150,21 +150,21 @@ new ScrollMagic.Scene({
 .addTo(controller);
 
 new ScrollMagic.Scene({
- triggerElement: '#photography',
+ triggerElement: '#album',
  duration: "50%",
  offset: -100 
 })
-.setTween("#photography_overlay", .5, {opacity:0, ease: Power1.easeOut})
-//.addIndicators({name:'photography_overlay_fadeIn',colorStart:'orange',colorTrigger:'purple' })
+.setTween("#album_overlay", .5, {opacity:0, ease: Power1.easeOut})
+//.addIndicators({name:'album_overlay_fadeIn',colorStart:'orange',colorTrigger:'purple' })
 .addTo(controller);
 
 new ScrollMagic.Scene({
- triggerElement: '#photography_summary',
+ triggerElement: '#album_summary',
  duration: "50%",
  offset: -100 
 })
-.setTween("#photography_overlay", .5, {opacity:1, ease: Power1.easeOut})
-//.addIndicators({name:'photography_overlay_fadeOut',colorStart:'purple',colorTrigger:'yellow' })
+.setTween("#album_overlay", .5, {opacity:1, ease: Power1.easeOut})
+//.addIndicators({name:'album_overlay_fadeOut',colorStart:'purple',colorTrigger:'yellow' })
 .addTo(controller);
 
 new ScrollMagic.Scene({
